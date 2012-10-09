c308213a242db99acd9e9224e89a86154625fe32
joseignaciosg <joseignaciosg9@gmail.com>
2012-10-09 20:29:12 -0300

    Scanner aded to getHeaders

--- src/main/java/parser/HeaderReader.java
+++ src/main/java/parser/HeaderReader.java
@@ -1,66 +1,58 @@
 package parser;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.regex.Matcher;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 import org.apache.log4j.Logger;
 
 public class HeaderReader {
 
-	private static Logger logger = Logger.getLogger(HeaderReader.class);
-	private String mail_string;
-	private File mail_file;
+    private static Logger logger = Logger.getLogger(HeaderReader.class);
+    private String mail_string;
+    private File mail_file;
 
-	public HeaderReader(final String mail) {
-		this.mail_string = mail;
+    public HeaderReader(final String mail) {
+	this.mail_string = mail;
+    }
+
+    public HeaderReader(final File mail) {
+	this.mail_file = mail;
+    }
+
+    public String getHeaders() throws IOException {
+	final StringBuilder builder = new StringBuilder();
+	// final BufferedReader reader = new BufferedReader(new FileReader(
+	// this.mail_string));
+	final Scanner scanner = new Scanner(this.mail_file);
+	final String boundary = this.getBoundary();
+	logger.debug("Reading boundary:" + boundary);
+
+	while (scanner.hasNextLine()) {
+	    final String line = scanner.nextLine();
+	    if (!Pattern.matches(".*--" + boundary + ".*", line)) {
+		builder.append(line + "\r\n");
+	    } else {
+		break;
+	    }
 	}
+	scanner.close();
+	logger.debug("Reading headers:");
+	return builder.toString();
+    }
 
-	public HeaderReader(final File mail) {
-		this.mail_file = mail;
+    private String getBoundary() throws IOException {
+	final Scanner scanner = new Scanner(this.mail_file);
+	while (scanner.hasNextLine()) {
+	    final String line = scanner.nextLine();
+	    final boolean isMatch = Pattern.matches(".*boundary.*", line);
+	    if (isMatch) {
+		scanner.close();
+		return line.split("\"")[1];
+	    }
 	}
-
-	public String getHeaders() throws IOException {
-		final StringBuilder builder = new StringBuilder();
-		// final BufferedReader reader = new BufferedReader(new FileReader(
-		// this.mail_string));
-		final BufferedReader reader = new BufferedReader(new FileReader(
-				this.mail_file));
-		final String boundary = this.getBoundary();
-		logger.debug("Reading boundary:" + boundary);
-
-		String line;
-		do {
-			line = reader.readLine();
-			builder.append(reader.readLine() + "\r\n");
-		} while (!Pattern.matches(".*--" + boundary + ".*", line));
-		reader.close();
-		logger.debug("Reading headers:");
-		return builder.toString();
-	}
-
-	private String getBoundary() throws IOException {
-		// final BufferedReader reader = new BufferedReader(new FileReader(
-		// this.mail_string));
-		final BufferedReader reader = new BufferedReader(new FileReader(
-				this.mail_file));
-		// final Pattern p = Pattern.compile(".*boundary//.*");
-		String line;
-		final Matcher m;
-		do {
-			line = reader.readLine();
-			final boolean isMatch = Pattern.matches(".*boundary.*", line);
-			// m = p.matcher(line);
-			// if (m.matches()) {
-			if (isMatch) {
-				reader.close();
-				return line.split("\"")[1];
-			}
-		} while (line != null);
-		reader.close();
-		return null;
-	}
+	scanner.close();
+	return null;
+    }
 }
--- src/main/java/server/start/ProxyInitializer.java
+++ src/main/java/server/start/ProxyInitializer.java
@@ -11,33 +11,33 @@ import worker.watcher.DirectoryWatcher;
 
 public class ProxyInitializer {
 
-	private static Logger logger = Logger.getLogger(ProxyInitializer.class);
+    private static Logger logger = Logger.getLogger(ProxyInitializer.class);
 
-	public static void main(final String[] args) {
-		new ProxyInitializer().initialize();
+    public static void main(final String[] args) {
+	new ProxyInitializer().initialize();
 
+    }
+
+    public void initialize() {
+	logger.trace("Initializing proxy.....");
+	new ServerInitializer().initialize("server.init");
+	this.initFileWatcher();
+	logger.trace("Proxy Started succesfully!");
+    }
+
+    private void initFileWatcher() {
+	try {
+	    final String[] dirs = { Config.getInstance().get(
+		    "specific_conf_dir") };
+	    final DirectoryWatcher watcher = new DirectoryWatcher(dirs, false);
+	    watcher.suscribe(StandardWatchEventKinds.ENTRY_CREATE,
+		    new ConfigWatcherCallback("Creation Listener"));
+	    watcher.suscribe(StandardWatchEventKinds.ENTRY_MODIFY,
+		    new ConfigWatcherCallback("Modification Listener"));
+
+	    new Thread(watcher).start();
+	} catch (final IOException e) {
+	    throw new IllegalStateException("Could not initialize file watcher");
 	}
-
-	public void initialize() {
-		logger.trace("Initializing proxy.....");
-		new ServerInitializer().initialize("server.init");
-		this.initFileWatcher();
-		logger.trace("Proxy Started succesfully!");
-	}
-
-	private void initFileWatcher() {
-		try {
-			final String[] dirs = { Config.getInstance().get(
-					"specific_conf_dir") };
-			final DirectoryWatcher watcher = new DirectoryWatcher(dirs, false);
-			watcher.suscribe(StandardWatchEventKinds.ENTRY_CREATE,
-					new ConfigWatcherCallback("Creation Listener"));
-			watcher.suscribe(StandardWatchEventKinds.ENTRY_MODIFY,
-					new ConfigWatcherCallback("Modification Listener"));
-
-			new Thread(watcher).start();
-		} catch (final IOException e) {
-			throw new IllegalStateException("Could not initialize file watcher");
-		}
-	}
+    }
 }
