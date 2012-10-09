7569257cf021eb6865f4682c5c49215f35351517
Gonzalo Castiglione <gonzalo@gonzalo-ubuntu.(none)>
2012-10-09 19:16:54 -0300

    corrijo detalles menores en clas TimeValidator

--- src/main/java/validator/TimeValidator.java
+++ src/main/java/validator/TimeValidator.java
@@ -1,39 +1,33 @@
 package validator;
 
+import org.apache.log4j.Logger;
 import org.joda.time.Interval;
 import org.joda.time.LocalTime;
 
 import util.Config;
 
 public class TimeValidator {
-	
-	private static Config accessTimeConfig = Config.getInstance().getConfig(
-			"access_time");
 
-	public boolean validate(String user){
-		
+	private static Logger logger = Logger.getLogger(TimeValidator.class);
+	private static Config accessTimeConfig = Config.getInstance().getConfig("access_time");
+
+	public boolean validate(String user) {
 		String timeRestriction = accessTimeConfig.get(user);
-		if(timeRestriction == null){ 
-			
-			// No restriction entries found for this user; access guaranteed
+		if (timeRestriction == null) {
 			return true;
 		}
-		
-		// It's important to remove all whitespaces and blanks so the date builders wont 
-		// throw unnecesary exceptions
-		// Maybe we should make the "replaceAll" in the get method of Config class		
-		timeRestriction = timeRestriction.replaceAll("\\s","");
-
-		String[] times = timeRestriction.split("-");
-		String[] startTimeParts = times[0].split(":");
-		String[] endTimeParts = times[1].split(":");
-		LocalTime startTime = new LocalTime(Integer.valueOf(startTimeParts[0]), Integer.valueOf(startTimeParts[1]));
-		LocalTime endTime = new LocalTime(Integer.valueOf(endTimeParts[0]), Integer.valueOf(endTimeParts[1]));
-		Interval timeRestrictionInterval = new Interval(startTime.toDateTimeToday(), endTime.toDateTimeToday());
-
-		// Finally we check if the current time is between the interval
-		return timeRestrictionInterval.containsNow();
-		
+		try {
+			timeRestriction = timeRestriction.trim();
+			String[] times = timeRestriction.split("-");
+			String[] startTimeParts = times[0].split(":");
+			String[] endTimeParts = times[1].split(":");
+			LocalTime startTime = new LocalTime(Integer.valueOf(startTimeParts[0]), Integer.valueOf(startTimeParts[1]));
+			LocalTime endTime = new LocalTime(Integer.valueOf(endTimeParts[0]), Integer.valueOf(endTimeParts[1]));
+			Interval timeRestrictionInterval = new Interval(startTime.toDateTimeToday(), endTime.toDateTimeToday());
+			return timeRestrictionInterval.containsNow();
+		} catch (Exception e) {
+			logger.error("Could not parse schedule for " + user + " - read: " + timeRestriction);
+			return false;
+		}
 	}
-	
 }
