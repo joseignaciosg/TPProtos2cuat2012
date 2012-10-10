84ac0166dde4789deae3cd3446419e306267ccbd
Gonzalo Castiglione <gonzalo@gonzalo-ubuntu.(none)>
2012-10-10 17:24:51 -0300

    elimino conflictos de archivo subido en conflitos

--- src/test/reader/HeaderReaderTest.java
+++ src/test/reader/HeaderReaderTest.java
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import junit.framework.Assert;
 
 import org.junit.Before;
@@ -6,38 +5,23 @@ import org.junit.Test;
 
 import parser.HeaderReader;
 import util.IOUtil;
-=======
+
 import src.main.java.parser.HeaderReader;
->>>>>>> Stashed changes
 
 public class HeaderReaderTest {
 
-    private HeaderReader reader;
-    private final String mail = "test_mail.txt";
-    String mail_path;
+	private HeaderReader reader;
+	private String mail = "test_mail.txt";
+	private String mail_path;
 
-<<<<<<< Updated upstream
 	@Before
 	public void init() {
 		mail_path = IOUtil.getResource(mail).getPath();
-		reader = new HeaderReader(this.mail_path);
+		reader = new HeaderReader(mail_path);
 	}
-=======
-    @Before
-    public void init() {
-	this.mail_path = this.getClass().getClassLoader()
-		.getResource(this.mail).getPath();
-	this.reader = new HeaderReader(this.mail_path);
-    }
->>>>>>> Stashed changes
 
-    @Test
-    public void getHeadersTest() {
-	try {
-	    Assert.assertTrue(this.reader.getHeaders() != null);
-	    System.out.println(this.reader.getHeaders());
-	} catch (final Exception e) {
-	    e.printStackTrace();
+	@Test
+	public void getHeadersTest() {
+		Assert.assertTrue(reader.getHeaders() != null);
 	}
-    }
 }
