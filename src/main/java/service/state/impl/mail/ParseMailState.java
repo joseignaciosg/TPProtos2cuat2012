c0a05bdb54260b7c8bc32cab3e80e5984956ed0d
Gonzalo Castiglione <gonzalo@gonzalo-ubuntu.(none)>
2012-10-14 15:11:03 -0300

    se estaba ejecutando el coamndo incorrecto al hacer quit.

--- src/main/java/service/state/impl/mail/ParseMailState.java
+++ src/main/java/service/state/impl/mail/ParseMailState.java
@@ -1,10 +1,10 @@
 package service.state.impl.mail;
 
 import service.AbstractSockectService;
-import service.command.impl.ExitCommand;
 import service.command.impl.mail.DefaultCommand;
 import service.command.impl.mail.DeleCommand;
 import service.command.impl.mail.EchoUntilPointCommand;
+import service.command.impl.mail.QuitCommand;
 import service.command.impl.mail.RetrCommand;
 import service.state.State;
 
@@ -17,7 +17,7 @@ public class ParseMailState extends State {
 		commandRecognizer.register("UIDL", EchoUntilPointCommand.class);
 		commandRecognizer.register("RETR", RetrCommand.class);
 		commandRecognizer.register("DELE", DeleCommand.class);
-		commandRecognizer.register("QUIT", ExitCommand.class);
+		commandRecognizer.register("QUIT", QuitCommand.class);
 		commandRecognizer.registerDefault(DefaultCommand.class);
 	}
 
