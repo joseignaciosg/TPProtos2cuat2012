a15753678f08bf606edf131564b105dc22d04495
Gonzalo Castiglione <gonzalo@gonzalo-ubuntu.(none)>
2012-10-22 20:11:43 -0300

    arreglo bug en Authcommand.

--- src/main/java/service/command/impl/mail/AuthCommand.java
+++ src/main/java/service/command/impl/mail/AuthCommand.java
@@ -11,58 +11,56 @@ import org.apache.log4j.Logger;
 import service.AbstractSockectService;
 import service.MailSocketService;
 import service.command.ServiceCommand;
+
 public class AuthCommand extends ServiceCommand {
 
 	private static final Logger logger = Logger.getLogger(AuthCommand.class);
-	
+
 	public AuthCommand(AbstractSockectService owner) {
 		super(owner);
 	}
 
 	@Override
-	public void execute(String[] params) {
+	public void execute(String[] params) throws Exception {
+		String resp = echoToOriginServerAndReadLine(getOriginalLine());
+		User user;
 		if (CollectionUtil.empty(params)) {
-			System.out.println("Parametros vacios!!");
+			owner.echoLine(resp);
 			return;
-		}
-		if ("PLAIN".equals(params[0].toUpperCase())) {
-			String resp = echoAndgetResponse(getOriginalLine());
+		} else if ("PLAIN".equals(params[0].toUpperCase())) {
 			owner.echoLine(resp);
-			String base64Credentials = readLineFromClient();
-			resp = echoAndgetResponse(base64Credentials);
+			String base64Credentials = owner.read().readLine();
+			resp = echoToOriginServerAndReadLine(base64Credentials);
 			owner.echoLine(resp);
-			if (resp.startsWith("+OK")) {
+			if (!resp.startsWith("+OK")) {
 				return;
 			}
-			User user = createUser(base64Credentials);
+			user = createUser(base64Credentials);
 			getBundle().put("auth_user", user);
+		} else if ("LOGIN".equals(params[0].toUpperCase())) {
+			owner.echoLine(resp);
+			String username = owner.read().readLine();
+			resp = echoToOriginServerAndReadLine(username);
+			owner.echoLine(resp);
+			String password = owner.read().readLine();
+			resp = echoToOriginServerAndReadLine(password);
+			owner.echo(resp);
+			if (!resp.startsWith("+OK")) {
+				return;
+			}
+			user = createUser(username, password);
 		} else {
 			logger.error("Unknown login type.");
+			owner.echoLine("-ERR");
+			return;
 		}
+		getBundle().put("auth_user", user);
 	}
-	
-	private String echoAndgetResponse(String line) {
+
+	private String echoToOriginServerAndReadLine(String line) throws IOException {
 		MailSocketService service = (MailSocketService) owner;
 		service.echoLineToOriginServer(line);
-		return readLineFromOriginServer();
-	}
-	
-	private String readLineFromClient() {
-		try {
-			return owner.read().readLine();
-		} catch (IOException e) {
-			logger.error("Could not read line from client");
-			throw new IllegalStateException();
-		}
-	}
-	
-	private String readLineFromOriginServer() {
-		try {
-			return ((MailSocketService) owner).readFromOriginServer().readLine();
-		} catch (IOException e) {
-			logger.error("Could not read line from origin server");
-			throw new IllegalStateException();
-		}
+		return service.readFromOriginServer().readLine();
 	}
 
 	private User createUser(String base64Credentials) {
@@ -70,5 +68,13 @@ public class AuthCommand extends ServiceCommand {
 		String decoded = new String((byte[]) obj);
 		String[] parts = decoded.split("\0");
 		return new User(parts[1], parts[2]);
+	}
+	
+	private User createUser(String base64Username, String base64Password) {
+		String decodedUsername = new String((byte[]) new Base64().decode(base64Username.getBytes()));
+		String decodedPasswd = new String((byte[]) new Base64().decode(base64Password.getBytes()));
+		String username = decodedUsername.split("\0")[0];
+		String passwd = decodedPasswd.split("\0")[0];
+		return new User(username, passwd);
 	}
 }
