240779253ed9192a0a956134654e15102a2eaab4
Gonzalo Castiglione <gonzalo@gonzalo-ubuntu.(none)>
2012-10-25 15:26:55 -0300

    CHINO POR FAVORRRRR arrgla el problema de la indentacion de los archivos!

--- src/main/java/service/command/impl/mail/RetrCommand.java
+++ src/main/java/service/command/impl/mail/RetrCommand.java
@@ -15,34 +15,34 @@ import service.command.ServiceCommand;
 
 public class RetrCommand extends ServiceCommand {
 
-    protected static final Logger logger = Logger.getLogger(DeleCommand.class);
+	protected static final Logger logger = Logger.getLogger(DeleCommand.class);
 
-    public RetrCommand(AbstractSockectService owner) {
-	super(owner);
-    }
+	public RetrCommand(AbstractSockectService owner) {
+		super(owner);
+	}
 
-    @Override
-    public void execute(String[] params) throws Exception {
-	boolean showToClient = true;
-	if (params.length == 3) {
-	    showToClient = Boolean.valueOf(params[3]);
+	@Override
+	public void execute(String[] params) throws Exception {
+		boolean showToClient = true;
+		if (params.length == 3) {
+			showToClient = Boolean.valueOf(params[3]);
+		}
+		MailSocketService mailSocketService = (MailSocketService) owner;
+		owner.echoLine(getOriginalLine());
+		BufferedReader breader = mailSocketService.read();
+		DataOutputStream outstream = null;
+		String firstLine = breader.readLine();
+		File mailContent;
+		if (showToClient) {
+			outstream = mailSocketService.getOutPutStream();
+		}
+		mailContent = mailSocketService.getMailRetriever().retrieve("mail", breader, outstream);
+		MailMimeParser parser = new MailMimeParser();
+		int sizeInBytes = Integer.valueOf(firstLine.split(" ")[1]);
+		Mail mail = parser.parse(mailContent, sizeInBytes,
+				mailSocketService.getMailTranformer());
+		if (!showToClient) {
+			getBundle().put("DELE_" + params[0], mail);
+		}
 	}
-	MailSocketService mailSocketService = (MailSocketService) owner;
-	owner.echoLine(getOriginalLine());
-	BufferedReader breader = mailSocketService.read();
-	DataOutputStream outstream = null;
-	String firstLine = breader.readLine();
-	File mailContent;
-	if (showToClient) {
-	    outstream = mailSocketService.getOutPutStream();
-	}
-	mailContent = mailSocketService.getMailRetriever().retrieve("mail",
-		breader, outstream);
-	MailMimeParser parser = new MailMimeParser();
-	int sizeInBytes = Integer.valueOf(firstLine.split(" ")[1]);
-	Mail mail = parser.parse(mailContent, sizeInBytes, mailSocketService.getMailTranformer());
-	if (!showToClient) {
-	    getBundle().put("DELE_" + params[0], mail);
-	}
-    }
 }
