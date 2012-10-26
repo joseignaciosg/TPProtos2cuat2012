58dab8f81b95eb190e15216bfa586c17d1feeec8
Gonzalo Castiglione <gonzalo@gonzalo-ubuntu.(none)>
2012-10-25 23:30:01 -0300

    IMposibel trabajar con el Anterior parser. Lo volvi a pensar bien y ahora quedo mucho mas entendible y ya esta listo para aplciarle las trasnformaciones.

--- src/main/java/model/parser/mime/MailMimeParser.java
+++ src/main/java/model/parser/mime/MailMimeParser.java
@@ -3,8 +3,8 @@ package model.parser.mime;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 import model.mail.Mail;
@@ -15,91 +15,92 @@ import org.apache.log4j.Logger;
 public class MailMimeParser {
 
 	protected static final Logger logger = Logger.getLogger(MailMimeParser.class);
-	
+
 	private MimeHeaderParser headerParser;
-		
+
 	public MailMimeParser() {
 		headerParser = new MimeHeaderParser();
 	}
-	
+
 	public Mail parse(File mimeFile, MailTransformer transformer) throws IOException {
 		return parse(mimeFile, mimeFile.length(), transformer);
 	}
-	
+
 	public Mail parse(File mimeFile, long sizeInBytes, MailTransformer transformer) throws IOException {
 		Mail mail = new Mail(mimeFile);
-		File transformedFile = File.createTempFile(mimeFile.getName(), "_transformed");
+		File transformedFile = File.createTempFile("transformed_", mimeFile.getName());
 		mail.setSizeInBytes(sizeInBytes);
 		Scanner scanner = new Scanner(mimeFile);
 		FileWriter transWriter = new FileWriter(transformedFile);
-		headerParser.parse(scanner, mail);
-		parsePart(scanner, mail.getBoundaryKey(), mail, transformer);
+		parseBoundary(scanner, mail);
 		scanner.close();
 		return mail;
 	}
 
-	private void parsePart(Scanner scanner, String boundary, Mail mail,MailTransformer transformer ) throws IOException{
-		boolean cont = false;
-	    	StringBuilder content = new StringBuilder();
-	    	String line = null;
-	    	List<MimeHeader> headers;
-	    	while (scanner.hasNextLine()) {
-	    	    	if(cont){
-	    	    	    	line  = scanner.nextLine();
-	    	    	    	cont = false;
-	    	    	}
-			if ( line.equals("--" + boundary)) {
-			    	// start of a segment
-				logger.info("PART START : " + boundary);
-				//parse header
-				headers = parsePartHeader(scanner, mail,transformer);
-				//parse content
-				while (scanner.hasNextLine()){
-				    String contLine = scanner.nextLine();
-				    if(contLine.equals("--" + boundary)){
-					cont = true;
-					break;
-				    }else if(line.equals("--" + boundary + "--")){
-					return;
-				    }	
-				    content.append(contLine);
-				}
-				content = transformer.transform(content, headers);
-				//TODO write to auxiliar File mail
-			} else if (line.equals("--" + boundary + "--")) {
-				logger.info("PART END : " + boundary);
-				// end of mail file
+	private void parseBoundary(Scanner scanner, Mail mail) throws IOException {
+		headerParser.parse(scanner, mail);
+		// TODO: hacer con los header todas las transformaciones pertinentes con el objeto MailTransformer
+		String boundary = mail.getBoundaryKey();
+		String boundaryLine = scanner.nextLine();
+		boolean endOfMail = false;
+		do {
+			logger.debug("Start Boundary: " + boundary);
+			parseBoundary(scanner, boundary, boundaryLine);
+			String line = scanner.nextLine();
+			endOfMail = line.equals(".");
+			if (!endOfMail && !line.startsWith("--" + boundary)) {
+				throw new IllegalStateException("Unexpected line: " + line + ". Expected end of bondary.");				
+			}
+		} while (!endOfMail);
+	}
+
+	private void parseBoundary(Scanner scanner, String boundary, String lastLine) throws IOException {
+		if (!lastLine.equals("--" + boundary)) {
+			throw new IllegalStateException("Expected beggining of boundary");
+		}
+		Map<String, MimeHeader> headers = readBoundaryHeaders(scanner);
+		MimeHeader contentType = headers.get("Content-Type");
+		if (contentType != null) {
+			String subBoundary = contentType.getExtraValue("boundary");
+			if (subBoundary != null) {
+				logger.info("Sub boundary " + subBoundary + " found. Recursively parsing.");
+				parseBoundary(scanner, subBoundary, scanner.nextLine());
 				return;
 			}
 		}
+		StringBuilder text = new StringBuilder();
+		boolean endOfBoundary = false;
+		String line;
+		do {
+			line = scanner.nextLine();
+			endOfBoundary = line.startsWith("--" + boundary);
+			if (!endOfBoundary) {
+				text.append(line + "\r\n");
+			}
+		} while (!endOfBoundary);
+		// TODO: hacer todas las transformacione pertinentes a text y header con el objeto MailTransformer
+		if (line.equals("--" + boundary)) {
+			parseBoundary(scanner, boundary, line);
+			return;
+		} else if (line.equals("--" + boundary + "--")) {
+			return;
+		} else {
+			throw new IllegalStateException("Unexpected line: " + line);
+		}
 	}
 
-
-
-	private List<MimeHeader> parsePartHeader(Scanner scanner, Mail mail,MailTransformer transformer ) throws IOException {
-		MimeHeader contentType = null;
-		 List<MimeHeader> ret = new ArrayList<MimeHeader>();
+	private Map<String, MimeHeader> readBoundaryHeaders(Scanner scanner) {
+		Map<String, MimeHeader> headers = new HashMap<String, MimeHeader>();
 		while (scanner.hasNextLine()) {
 			String line = scanner.nextLine();
-			// according to rfc, an empty line marks the of sub-boundary headers 
+			// according to rfc, an empty line marks the of sub-boundary headers
 			if (line.equals("")) {
 				break;
 			}
 			MimeHeader header = new MimeHeader(line);
-			mail.addAttachmentsExtension(header.getValue());
-			if (header.getKey().equals("Content-Type")) {
-				contentType = header;
-				ret.add(contentType);
-				
-			}
+			headers.put(header.getKey(), header);
+			logger.debug("Parsed boundary header => " + header);
 		}
-		if (contentType != null) {
-			String subBoundary = contentType.getExtraValue("boundary");
-			if (subBoundary != null) {				
-				logger.info("Sub boundary " + subBoundary + " found. Recursively parsing.");
-				parsePart(scanner, subBoundary, mail,transformer);
-			}
-		}
-		return ret;
+		return headers;
 	}
 }
