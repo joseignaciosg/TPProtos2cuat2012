d71479d63df1e5e4e764056c2d9b773caaccb560
joseignaciosg <joseignaciosg9@gmail.com>
2012-10-28 18:04:51 -0300

    merge

--- src/main/java/model/mail/transformerimpl/ImageTransformer2.java
+++ src/main/java/model/mail/transformerimpl/ImageTransformer2.java
@@ -14,6 +14,7 @@ import org.apache.commons.codec.binary.Base32InputStream;
 import model.parser.mime.ContentTypeUtil;
 import model.parser.mime.MimeHeader;
 import model.util.Base64Util;
+import model.util.IOUtil;
 import model.util.ImageRotator;
 
 import org.apache.commons.codec.binary.Base64;
@@ -22,57 +23,63 @@ public class ImageTransformer2 implements Transformer {
 
 	private ImageRotator rotator = new ImageRotator();
 	private static final List<String> availableTypes = new LinkedList<String>();
-	
+
 	static {
 		availableTypes.add(ContentTypeUtil.getContentType("jpg"));
 		availableTypes.add(ContentTypeUtil.getContentType("jpeg"));
 		availableTypes.add(ContentTypeUtil.getContentType("png"));
 	}
-	
+
 	@Override
-	public StringBuilder transform(StringBuilder part, Map<String, MimeHeader> partheaders) throws IOException {
+	public StringBuilder transform(StringBuilder part,
+			Map<String, MimeHeader> partheaders) throws IOException {
 		MimeHeader contentType = partheaders.get("Content-Type");
-		MimeHeader contentTransferEncoding = partheaders.get("Content-Transfer-Encoding");
-		if (contentType == null || !availableTypes.contains(contentType.getValue())) {
+		MimeHeader contentTransferEncoding = partheaders
+				.get("Content-Transfer-Encoding");
+		if (contentType == null
+				|| !availableTypes.contains(contentType.getValue())) {
 			return part;
 		}
-		if("base64".equals(contentTransferEncoding)){
-		    try {
-			File originalImage = getUnencodedImage(part);
-			File rotatedImage = rotator.createRotatedImage(ImageIO.read(originalImage));
-			File encodedImage = Base64Util.encodeUsingOS(rotatedImage);
-			StringBuilder rotatedEncodedImage = new StringBuilder();
-			Scanner scanner = new Scanner(encodedImage);
-			while (scanner.hasNextLine()) {
-			    rotatedEncodedImage.append(scanner.nextLine() + "\r\n");
+		if ("base64".equals(contentTransferEncoding)) {
+			try {
+				File originalImage = getUnencodedImage(part);
+				File rotatedImage = rotator.createRotatedImage(ImageIO
+						.read(originalImage));
+				File encodedImage = Base64Util.encodeUsingOS(rotatedImage);
+				StringBuilder rotatedEncodedImage = new StringBuilder();
+				Scanner scanner = new Scanner(encodedImage);
+				while (scanner.hasNextLine()) {
+					rotatedEncodedImage.append(scanner.nextLine() + "\r\n");
+				}
+				encodedImage.delete();
+				rotatedImage.delete();
+				originalImage.delete();
+				scanner.close();
+				return rotatedEncodedImage;
+			} catch (InterruptedException e) {
+				throw new IllegalStateException(e);
 			}
-			encodedImage.delete();
-			rotatedImage.delete();
-			originalImage.delete();
-			scanner.close();
-			return rotatedEncodedImage;
-		    } catch (InterruptedException e) {
-			throw new IllegalStateException(e);
-		    }
-		}else{
-		    File originalImage;
-		    try {
-			originalImage = getImage(part);
-			File rotatedImage = rotator.createRotatedImage(ImageIO.read(originalImage));
-		    } catch (InterruptedException e) {
-			e.printStackTrace();
-		    }
+		} else {
+			File originalImage;
+			try {
+				originalImage = getImage(part);
+				File rotatedImage = rotator.createRotatedImage(ImageIO.read(originalImage));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 		return part;
 	}
-	
-	private File getUnencodedImage(StringBuilder encodedText) throws IOException, InterruptedException {
-	    return Base64Util.decodeUsingOS(encodedText.toString().replace("\r", ""));
+
+	private File getUnencodedImage(StringBuilder encodedText)
+			throws IOException, InterruptedException {
+		return Base64Util.decodeUsingOS(encodedText.toString()
+				.replace("\r", ""));
 	}
 
-	private File getImage(StringBuilder encodedText) throws IOException, InterruptedException {
-	    return null;
+	private File getImage(StringBuilder encodedText) throws IOException,
+			InterruptedException {
+		return IOUtil.createFileWithContents(encodedText.toString());
 	}
 
-	
 }
