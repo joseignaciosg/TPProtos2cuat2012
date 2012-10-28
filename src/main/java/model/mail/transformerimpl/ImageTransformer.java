cbbc0d4ec61ca74995aedb1c74148ab6a91bb8a9
Gonzalo Castiglione <gonzalo@gonzalo-ubuntu.(none)>
2012-10-27 22:49:50 -0300

    chino no me pgues... realmente no peudo seguir la linea de ejecucion con el codigo mal tabulado!

--- src/main/java/model/mail/transformerimpl/ImageTransformer.java
+++ src/main/java/model/mail/transformerimpl/ImageTransformer.java
@@ -20,90 +20,90 @@ import model.parser.mime.MimeHeader;
 
 import org.apache.commons.codec.binary.Base64;
 
-
 public class ImageTransformer implements Transformer {
-    
-    String[] supportedImageTypes = {"jpeg","rgb","tiff","xbm","png","jpg"} ;
 
-    @Override
-    public StringBuilder transform(StringBuilder text, Map<String, MimeHeader> partheaders) throws IOException {
-	MimeHeader contentType = partheaders.get("Content-Type");
-	MimeHeader contentTransferEncoding = partheaders.get("Content-Transfer-Encoding");
-	StringBuilder ret = new StringBuilder();
-	if (contentType.getValue().startsWith("image/")) {
-	    	File inimage = File.createTempFile("transformed_", "sourceimage");
-	    	FileWriter fr = new FileWriter(inimage);
-		    fr.write(text.toString() + "\r\n");
-		fr.close();
-		//si se hace base64 -D inimage , da la imagen bien
-	    	inimage = decodeBase64(text.toString(), contentTransferEncoding, inimage);
-		InputStream is=null;
-		if (inimage != null){
-		    try {
-			is = new BufferedInputStream(new FileInputStream(
-				inimage));
-		    } catch (FileNotFoundException e) {
-			e.printStackTrace();
-		    }
-		}
-		BufferedImage img = null;
-		final ByteArrayOutputStream out = new ByteArrayOutputStream();
-		try {
-			img = ImageIO.read(is);
-		} catch (final IOException e) {
-			e.printStackTrace();
-		}
-		final int w = img.getWidth();
-		final int h = img.getHeight();
-		final BufferedImage dimg = new BufferedImage(w, h, img.getColorModel()
-				.getTransparency());
-		final Graphics2D g = dimg.createGraphics();
-		g.drawImage(img, 0, 0, w, h, 0, h, w, 0, null);
-		g.dispose();
-		try {
-			ImageIO.write(dimg, "png", out);
-		} catch (final IOException e) {
-			e.printStackTrace();
-		}
-		out.toByteArray();
-		final File outimage = File.createTempFile("transformed_", "retimage");
-		try {
-			ImageIO.write(dimg, "png", outimage);
-		} catch (final IOException e) {
-			e.printStackTrace();
-		}
-		
-		Scanner scanner = new Scanner(outimage);
-		while(scanner.hasNextLine()){
-		    ret.append(scanner.nextLine() + "\r\n");
-		}
-		scanner.close();
-		return ret;
-	}
-	return text;
-    }
+	String[] supportedImageTypes = { "jpeg", "rgb", "tiff", "xbm", "png", "jpg" };
 
-    private File decodeBase64(String image, MimeHeader contentTransferEncoding, File inimage) {
-	StringBuilder tobedecoded = new StringBuilder();
-	String[] lines = image.split("\r\n");
-	for(String line: lines){
-	    tobedecoded.append(line);
+	@Override
+	public StringBuilder transform(StringBuilder text,
+			Map<String, MimeHeader> partheaders) throws IOException {
+		MimeHeader contentType = partheaders.get("Content-Type");
+		MimeHeader contentTransferEncoding = partheaders.get("Content-Transfer-Encoding");
+		StringBuilder ret = new StringBuilder();
+		if (contentType.getValue().startsWith("image/")) {
+			File inimage = File.createTempFile("transformed_", "sourceimage");
+			FileWriter fr = new FileWriter(inimage);
+			fr.write(text.toString() + "\r\n");
+			fr.close();
+			// si se hace base64 -D inimage , da la imagen bien
+			inimage = decodeBase64(text.toString(), contentTransferEncoding,
+					inimage);
+			InputStream is = null;
+			if (inimage != null) {
+				try {
+					is = new BufferedInputStream(new FileInputStream(inimage));
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
+			BufferedImage img = null;
+			final ByteArrayOutputStream out = new ByteArrayOutputStream();
+			try {
+				img = ImageIO.read(is);
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+			final int w = img.getWidth();
+			final int h = img.getHeight();
+			final BufferedImage dimg = new BufferedImage(w, h, img.getColorModel().getTransparency());
+			final Graphics2D g = dimg.createGraphics();
+			g.drawImage(img, 0, 0, w, h, 0, h, w, 0, null);
+			g.dispose();
+			try {
+				ImageIO.write(dimg, "png", out);
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+			out.toByteArray();
+			final File outimage = File.createTempFile("transformed_",
+					"retimage");
+			try {
+				ImageIO.write(dimg, "png", outimage);
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+
+			Scanner scanner = new Scanner(outimage);
+			while (scanner.hasNextLine()) {
+				ret.append(scanner.nextLine() + "\r\n");
+			}
+			scanner.close();
+			return ret;
+		}
+		return text;
 	}
-	if(contentTransferEncoding.getValue().startsWith("base64")){
-        	Base64 base64 = new Base64();
-        	String decoded = new String(base64.decode(tobedecoded.toString()));
-        	try {
-        	    final File decodedinimage = File.createTempFile("transformed_", "sourceimage");
-        	    BufferedWriter filew = new BufferedWriter( new FileWriter( decodedinimage));
-        	    filew.write( decoded );
-        	    filew.close();
-        	    return decodedinimage;
-        	} catch (IOException e) {
-        	    e.printStackTrace();
-        	}
+
+	private File decodeBase64(String image, MimeHeader contentTransferEncoding, File inimage) {
+		StringBuilder tobedecoded = new StringBuilder();
+		String[] lines = image.split("\r\n");
+		for (String line : lines) {
+			tobedecoded.append(line);
+		}
+		if (contentTransferEncoding.getValue().startsWith("base64")) {
+			Base64 base64 = new Base64();
+			String decoded = new String(base64.decode(tobedecoded.toString()));
+			try {
+				final File decodedinimage = File.createTempFile("transformed_", "sourceimage");
+				BufferedWriter filew = new BufferedWriter(new FileWriter(decodedinimage));
+				filew.write(decoded);
+				filew.close();
+				return decodedinimage;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return inimage;
+
 	}
-	return inimage;
-	
-    }
 
 }
