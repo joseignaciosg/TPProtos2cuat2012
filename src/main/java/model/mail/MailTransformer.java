a2139c41493b13f73e64b958b8dac233897f461a
Gonzalo Castiglione <gonzalo@gonzalo-ubuntu.(none)>
2012-10-27 22:15:49 -0300

    El mailtrasnformer estaba pasando el aprametro incorrecto alos transfomers.

--- src/main/java/model/mail/MailTransformer.java
+++ src/main/java/model/mail/MailTransformer.java
@@ -34,7 +34,7 @@ public class MailTransformer {
 		StringBuilder retPart = part;
 		List<Transformer> transformers = getTransformerList();
 		for (Transformer transformer : transformers) {
-		    retPart = transformer.transform(part, partHeaders);
+		    retPart = transformer.transform(retPart, partHeaders);
 		}
 		return retPart;
 	}
