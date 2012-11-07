a799e84fcd08e31bbaaa8dd5397ec2d30e648acc
Alan Karpovsky <akarpovs@alu.itba.edu.ar>
2012-11-07 00:52:22 -0300

    Fixed - TimeValidator

--- src/main/java/model/validator/loginvalidator/TimeValidator.java
+++ src/main/java/model/validator/loginvalidator/TimeValidator.java
@@ -6,6 +6,7 @@ import model.validator.LoginValidationException;
 import model.validator.LoginValidator;
 
 import org.apache.log4j.Logger;
+import org.joda.time.DateTime;
 import org.joda.time.Interval;
 import org.joda.time.LocalTime;
 
@@ -32,11 +33,17 @@ public class TimeValidator implements LoginValidator {
 			final String[] times = timeRestriction.split("-");
 			final String[] startTimeParts = times[0].split(":");
 			final String[] endTimeParts = times[1].split(":");
-			final LocalTime startTime = new LocalTime(Integer.valueOf(startTimeParts[0]), Integer.valueOf(startTimeParts[1]));
-			final LocalTime endTime = new LocalTime(Integer.valueOf(endTimeParts[0]), Integer.valueOf(endTimeParts[1]));
-			final Interval timeRestrictionInterval = new Interval(startTime.toDateTimeToday(), endTime.toDateTimeToday());
-			if (timeRestrictionInterval.containsNow()) {
-				throw new LoginValidationException(mailAddress + " does not have access during this time");
+			final LocalTime startTime = new LocalTime(Integer.valueOf(startTimeParts[0].trim()), Integer.valueOf(startTimeParts[1].trim()));
+			LocalTime endTime = new LocalTime(Integer.valueOf(endTimeParts[0].trim()), Integer.valueOf(endTimeParts[1].trim()));
+			LocalTime now = LocalTime.now();
+			if(startTime.isAfter(endTime)){
+				if (now.isBefore(startTime) && now.isAfter(endTime)) {
+					throw new LoginValidationException(mailAddress + " does not have access during this time");
+				}
+			}else{
+				if (now.isBefore(startTime) || now.isAfter(endTime)) {
+					throw new LoginValidationException(mailAddress + " does not have access during this time");
+				}
 			}
 		} catch (final Exception e) {
 			if (e instanceof LoginValidationException) {
