1e7197e5098cbbe367b9bed0b5fe8d236a05f660
Gonzalo Castiglione <gonzalo@gonzalo-ubuntu.(none)>
2012-11-07 23:40:51 -0300

    header patter ahora busca por contains en vez de por equals

--- src/main/java/model/mail/MimeHeaderCollection.java
+++ src/main/java/model/mail/MimeHeaderCollection.java
@@ -8,19 +8,19 @@ import model.parser.mime.MimeHeader;
 public class MimeHeaderCollection {
 
 	private Map<String, MimeHeader> headers;
-	
+
 	public MimeHeaderCollection() {
 		headers = new HashMap<String, MimeHeader>();
 	}
-	
+
 	public void add(MimeHeader header) {
 		headers.put(header.getKey().toLowerCase(), header);
 	}
-	
+
 	public MimeHeader get(String key) {
 		return headers.get(key.toLowerCase());
 	}
-	
+
 	public boolean contains(String key, String value) {
 		MimeHeader header = get(key);
 		if (header == null) {
@@ -30,5 +30,13 @@ public class MimeHeaderCollection {
 			return header.getValue() == null;
 		}
 		return header.getValue().toLowerCase().equals(value.toLowerCase());
+	}
+
+	public boolean headerMatches(String key, String match) {
+		MimeHeader header = get(key);
+		if (header == null) {
+			return false;
+		}
+		return header.getValue().toLowerCase().contains(match.toLowerCase());
 	}
 }
