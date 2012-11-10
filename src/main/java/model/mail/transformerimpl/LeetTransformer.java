ea39f0777a282f52beb5092b394e101588b861d5
Gonzalo Castiglione <gonzalo@gonzalo-ubuntu.(none)>
2012-11-10 12:34:52 -0300

    Cuando el header content-type no viene. El contenido es texto.

--- src/main/java/model/mail/transformerimpl/LeetTransformer.java
+++ src/main/java/model/mail/transformerimpl/LeetTransformer.java
@@ -5,86 +5,81 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
+import org.apache.log4j.Logger;
+
 import model.mail.MimeHeaderCollection;
 import model.parser.mime.MimeHeader;
 import model.util.Base64Util;
 
 public class LeetTransformer implements Transformer {
 
+	private static final Logger logger = Logger.getLogger(LeetTransformer.class);
+	
 	@Override
 	public StringBuilder transform(StringBuilder text, MimeHeaderCollection partheaders) throws IOException {
 		MimeHeader contentType = partheaders.get("Content-Type");
-		if (contentType == null || !contentType.getValue().toLowerCase().startsWith("text/plain")) {
+		if (contentType != null && !contentType.getValue().toLowerCase().startsWith("text/plain")) {
 			return text;
 		}
 		MimeHeader contentTransferEncoding = partheaders.get("Content-Transfer-Encoding");
 		if (contentTransferEncoding == null) {
 			return new StringBuilder(toLeet(text.toString()));
 		} else {
-			if ("base64".equals(contentTransferEncoding.getValue().toLowerCase())) {
+			String encoding = contentTransferEncoding.getValue();
+			if ("base64".equalsIgnoreCase(encoding)) {
 				File encodedFile = File.createTempFile("encode_", ".tmp");
 				FileWriter encodedContentsWriter = new FileWriter(encodedFile);
 				try {
-					File decodedFile = Base64Util.decodeUsingOS(text
-							.toString());
+					File decodedFile = Base64Util.decodeUsingOS(text.toString());
 					Scanner decodedContents = new Scanner(decodedFile);
 					while (decodedContents.hasNextLine()) {
-						encodedContentsWriter.append(toLeet(decodedContents
-								.nextLine() + "\r\n"));
+						encodedContentsWriter.append(toLeet(decodedContents.nextLine()) + "\r\n");
 					}
 					decodedContents.close();
 					encodedContentsWriter.close();
-					File encodedText = Base64Util.encodeToFile(encodedFile);
-					Scanner encodedTextScaner = new Scanner(encodedText);
+					File base64EncodedFile = Base64Util.encodeToFile(encodedFile);
+					Scanner encodedTextScaner = new Scanner(base64EncodedFile);
 					StringBuilder builder = new StringBuilder();
 					while (encodedTextScaner.hasNextLine()) {
-						String line = encodedTextScaner.nextLine();
-						builder.append(line);
+						builder.append(encodedTextScaner.nextLine());
 					}
 					encodedTextScaner.close();
 					return builder;
 				} catch (InterruptedException e) {
 					throw new IllegalStateException(e);
 				}
-			} else { // quoted-printable
-				String transformed = toQuoted(text.toString());
-				return new StringBuilder(transformed);
+			} else if ("quoted-printable".equalsIgnoreCase(encoding)) {
+				return new StringBuilder(leetQuoted(text.toString()));
+			} else {
+				logger.error("Unknown encoding " + encoding + ". No changed done to the text");
+				return text;
 			}
 		}
 	}
-	
-	private String toQuoted(String decodeString ){
-		byte[] aux = decodeString.getBytes();
-		for(int i=0 ; i<aux.length; i++){
-			switch(aux[i]){
-			case 'a':
-				aux[i] = '4';
-				break;
-			case 'e':
-				aux[i] = '3';
-				break;
-			case 'i':
-				aux[i] = '1';
-				break;
-			case 'o':
-				aux[i] = '0';
-				break;
-			case '=':
-				i+=2;
-				break;
+
+	private String leetQuoted(String decodeString) {
+		char[] chars = decodeString.toCharArray();
+		String quotedLeet = "";
+		for (int i = 0; i < chars.length; i++) {
+			switch (chars[i]) {
+				case '=':
+					quotedLeet += chars[i] + "" + chars[i + 1] + "" + chars[i + 2];
+					i += 2;
+					break;
+				default:
+					quotedLeet += toLeet(String.valueOf(chars[i]));
 			}
 		}
-		return new String(aux);
+		return quotedLeet;
 	}
 
 	private String toLeet(String text) {
-		String textString = text;
-		textString = textString.replace("a", "4");
-		textString = textString.replace("e", "3");
-		textString = textString.replace("i", "1");
-		textString = textString.replace("o", "0");
-		return textString;
+		String leetText = text;
+		leetText = leetText.replace("a", "4");
+		leetText = leetText.replace("e", "3");
+		leetText = leetText.replace("i", "1");
+		leetText = leetText.replace("o", "0");
+		return leetText;
 	}
-
 
 }
