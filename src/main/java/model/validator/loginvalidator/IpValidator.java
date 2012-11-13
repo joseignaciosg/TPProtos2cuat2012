d2ab2abb3ef0a09e981f7996eac4de5ee6873261
Gonzalo Castiglione <gonzalo@gonzalo-ubuntu.(none)>
2012-11-13 00:15:34 -0300

    agrego catch agarrando todos cassos en lso que una ip no pudo ser parseada

--- src/main/java/model/validator/loginvalidator/IpValidator.java
+++ src/main/java/model/validator/loginvalidator/IpValidator.java
@@ -8,9 +8,12 @@ import model.validator.LoginValidationException;
 import model.validator.LoginValidator;
 
 import org.apache.commons.net.util.SubnetUtils;
+import org.apache.log4j.Logger;
 
 public class IpValidator implements LoginValidator {
 
+	private static final Logger logger = Logger.getLogger(IpValidator.class);
+	
 	private String userIp;
 	
 	public IpValidator(String userIp) {
@@ -23,16 +26,20 @@ public class IpValidator implements LoginValidator {
 		Collection<String> bannedList = bannedListConfig.getValues();
 		boolean isClientIpBanned = false;
 		for (String ip : bannedList) {
-			String[] inetAddressParts = ip.split("/");
-			if (inetAddressParts.length < 2) { // No subnet mask declaration
-				isClientIpBanned = userIp.equals(inetAddressParts[0]);
-			} else {
-				// access_ip.conf has an ip with subnet mask declaration
-				// (i.e. 200.232.44.0/16 )
-				isClientIpBanned = new SubnetUtils(ip).getInfo().isInRange(userIp);
-			}
-			if (isClientIpBanned) {
-				throw new LoginValidationException("IP " + userIp + " is banned from the server.");
+			try {
+				String[] inetAddressParts = ip.split("/");
+				if (inetAddressParts.length < 2) { // No subnet mask declaration
+					isClientIpBanned = userIp.equals(inetAddressParts[0]);
+				} else {
+					// access_ip.conf has an ip with subnet mask declaration
+					// (i.e. 200.232.44.0/16 )
+					isClientIpBanned = new SubnetUtils(ip).getInfo().isInRange(userIp);
+				}
+				if (isClientIpBanned) {
+					throw new LoginValidationException("IP " + userIp + " is banned from the server.");
+				}
+			} catch (IllegalArgumentException e) {
+				logger.error("Could not parse Ip " + ip + ". " + e.getMessage());
 			}
 		}
 	}
